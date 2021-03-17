e8e16295b045ce474c99f989b24484ceda7f700f
MasonPles <masonples@gmail.com>
2021-03-17 14:29:40 -0600

    Added profile dropdown and darkmode

---
+++ script/360_mainpage.js
@@ -0,0 +1,69 @@
+var nightMode = false;
+
+function dropdown(){
+    document.getElementById("myDropdown").classList.toggle("show")
+}
+
+function dropdown1(){
+    document.getElementById("myDropdown1").classList.toggle("show")
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.usericon')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+function toggleNightMode() {
+
+    document.body.classList.toggle("dark-mode");
+    document.getElementById("header").classList.toggle("dark-mode");
+
+    var button = document.getElementById("nightMode");
+    var logo = document.getElementById("logo");
+
+    if (nightMode == false) {
+
+        elements = document.getElementsByTagName("h3");
+        for (var i = 0; i < elements.length; i++) {
+        elements[i].style.color="white";
+        }
+
+        elements = document.getElementsByClassName("searchbar");
+        for (var i = 0; i < elements.length; i++) {
+        elements[i].style.background="#1f1f1f";
+        elements[i].style.color="white";
+        }
+
+        button.innerHTML = "Day Mode‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎";
+        logo.src = "images/lightRabbit.png";
+        nightMode = true;
+    }   
+
+    else {
+
+        elements = document.getElementsByTagName("h3");
+        for (var i = 0; i < elements.length; i++) {
+        elements[i].style.color="black";
+        }
+
+        elements = document.getElementsByClassName("searchbar");
+        for (var i = 0; i < elements.length; i++) {
+        elements[i].style.background="white";
+        elements[i].style.color="black";
+        }
+
+        button.innerHTML = "Night Mode";
+        logo.src = "images/rabbit.png";
+        nightMode = false;
+    }
+
+
+}
