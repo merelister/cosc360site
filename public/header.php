72b9849008b8c85604f4f1f881523310e4b72090
meredith <listermeredith@gmail.com>
2021-04-06 19:57:45 -0700

    Move frontend work to vanilla php backend

---
+++ public/header.php
@@ -0,0 +1,18 @@
+<?php $header = '<header id="header">
+            <a href="360_site.php" style="position: relative; z-index : 10;"><img src="images/rabbit.png" id="logo"></a>
+
+            <div class="dropdown">
+                <a onclick="dropdown()"><img src="https://thispersondoesnotexist.com/image" class="usericon"></a>
+                <div id="myDropdown" class="dropdown-content">
+                    <a href="profile_page.html">Profile</a>
+                    <a href="#">➕ New Post</a>
+                    <a onclick="toggleNightMode()" id="nightMode">Night Mode</a>
+                </div>
+            </div>
+
+            <form class="example" action="search.php" method="GET">
+                <input type="text" placeholder="Search.." name="search" class="searchbar">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+        </header>'
+?>
