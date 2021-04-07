503131d0078e5b8f35430f0865b9474cd9c9eb92
meredith <listermeredith@gmail.com>
2021-04-06 20:21:24 -0700

    Connect to database

---
+++ public/script/connect.php
@@ -0,0 +1,21 @@
+<?php
+    // connect to db
+    $host = "localhost";
+    $database = "360site";
+    $user = "webuser";
+    $password = "P@ssw0rd";
+
+$connection = mysqli_connect($host, $user, $password, $database);
+
+$error = mysqli_connect_error();
+if($error != null)
+{
+  $output = "<p>Unable to connect to database!</p>";
+  exit($output);
+}
+
+function query($sql) {
+    return mysqli_query($connection, $sql);
+}
+mysqli_close($connection);
+?>
